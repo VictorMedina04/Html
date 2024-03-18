78396da737bd20669e7fb5093c25dc4408776abc
Victor <medina.levic24@triana.salesianos.edu>
2024-03-18 10:05:34 +0100

    WE

--- Medina_Lejeune_Victor_B8/ejercicio06/ejercicio6.js
+++ Medina_Lejeune_Victor_B8/ejercicio06/ejercicio6.js
@@ -1,17 +1,11 @@
 let dni = prompt("Introduzca el numero del dni para calcular la letra");
 
-if (dni > 99999999 && dni < 0) {
-    alert("Introduzca un número valido");
+if (dni > 99999999 || dni < 0) {
+    prompt("Introduzca un número valido");
 } else {
-    function calcularLetraDni(dni) {
-
-        let letra = dni.charArt(8);
-        let numeros = dni.substring(0,7)
-        let letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", " D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
-        numeros = parseInt(numeros);
-        let letraDevuelta = numeros % (cadena.length - 1);
-        return letraDevuelta;
-    }
-
-    alert(calcularLetraDni);
+    let letra = dni.substring(dni.length - 1);
+    let numeros = dni.substring(0, dni.length - 1);
+    let letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", " D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+    numeros = parseInt(numeros);
+    let letraDevuelta = numeros % (cadena.length - 1);
 }
